89967d5b53575e9b6c20b5f8f0eede367900211b
davegrnr <davegrnr@gmail.com>
2021-05-12 14:13:54 -0400

    refactored to class components

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import JokeList from "./JokeList";
+import RefactoredJokeList from "./Refactored/JokeList";
+
+function App() {
+  return (
+    <div className="App">
+      <RefactoredJokeList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Refactored/JokeList.js
@@ -0,0 +1,96 @@
+import React, { Component } from "react";
+import axios from "axios";
+import Joke from "./Joke";
+import "./JokeList.css";
+
+class RefactoredJokeList extends Component {
+    static defaultProps = {
+        numJokes: 10
+    };
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            jokes: []
+        }
+        this.generateNewJokes = this.generateNewJokes.bind(this)
+        this.vote = this.vote.bind(this)
+    }
+
+    componentDidMount() {
+        if (this.state.jokes.length < this.props.numJokes) this.getJokes();
+    }
+
+    componentDidUpdate() {
+        if (this.state.jokes.length < this.props.numJokes) this.getJokes()
+    }
+
+    // get jokes from api
+
+    async getJokes() {
+        try{
+            let jokes = this.state.jokes;
+            let seenJokes = new Set(jokes.map(j => j.id))
+
+            while(jokes.length < this.props.numJokes) {
+                let res = await axios.get("https://icanhazdadjoke.com", {
+                    headers: { Accept: "application/json" }
+                });
+                let {status, ...joke} = res.data;
+
+                if(!seenJokes.has(joke.id)) {
+                    seenJokes.add(joke.id);
+                    jokes.push({...joke, votes: 0 })
+                } else {
+                    console.log("duplicate found")
+                }
+            }
+
+            this.setState({ jokes })
+        } catch(e) {
+            console.log(e)
+        }
+    }
+
+    generateNewJokes() {
+        this.setState({
+            jokes: []
+        })
+    }
+
+    vote(id, delta) {
+        this.setState(st => ({
+            jokes: st.jokes.map(j =>
+                j.id === id ? {...j, votes: j.votes + delta} : j)
+        }))
+    }
+
+    render() {
+        let sortedJokes = [...this.state.jokes].sort((a, b) => b.votes - a.votes)
+
+        return (
+            <div className="JokeList">
+                <button className="JokeList-getmore" onClick={this.generateNewJokes}>
+                    Get New Jokes
+                </button>
+            {sortedJokes.map(j => (
+                <Joke 
+                    text={j.joke}
+                    key={j.id}
+                    id={j.id}
+                    votes={j.votes}
+                    vote={this.vote}
+                />
+            ))}
+
+            {sortedJokes.length < this.props.numJokes ? (
+                <div className="loading">
+                    <i className="fas fa-4x fa-spinner fa-spin" />
+                </div>
+                ) : null}
+            </div>
+        )
+    }
+}
+
+export default RefactoredJokeList
